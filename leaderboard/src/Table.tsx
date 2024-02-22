ccb7a383fac8e29cb830dfb8492de45641087adf
pierrbt <pierre08bt@gmx.co.uk>
2024-02-22 20:33:14 +0100

    chore: fmt and lint leaderboard

--- leaderboard/src/Table.tsx
+++ leaderboard/src/Table.tsx
@@ -1,25 +1,24 @@
-import {Score} from "./types.ts";
+import { Score } from "./types.ts";
 
-
-function Table({scores}: { scores: Score[] }) {
-  return (
-    <table>
-      <thead>
-      <tr>
-        <th>Username</th>
-        <th>CPS</th>
-      </tr>
-      </thead>
-      <tbody>
-      {scores.map((score) => (
-        <tr key={score.id}>
-          <td>{score.username}</td>
-          <td>{score.cps.toFixed(2)}</td>
-        </tr>
-      ))}
-      </tbody>
-    </table>
-  )
+function Table({ scores }: { scores: Score[] }) {
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Username</th>
+					<th>CPS</th>
+				</tr>
+			</thead>
+			<tbody>
+				{scores.map((score) => (
+					<tr key={score.id}>
+						<td>{score.username}</td>
+						<td>{score.cps.toFixed(2)}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 }
 
-export default Table
+export default Table;
--- leaderboard/src/main.tsx
+++ leaderboard/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Leaderboard from './Leaderboard.tsx'
-import './css/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Leaderboard from "./Leaderboard.tsx";
+import "./css/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Leaderboard />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<Leaderboard />
+	</React.StrictMode>,
+);
