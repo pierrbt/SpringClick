74f7fb98f7db1b3d642287481cdd3d79f74bd99d
pierrbt <pierre08bt@gmx.co.uk>
2024-02-22 22:21:32 +0100

    fix: changes on leaderboard

--- leaderboard/index.js
+++ leaderboard/index.js
@@ -2,13 +2,14 @@ import {io} from "socket.io-client"
 
 const socket = io("ws://localhost:3000", {});
 
-let counter = 0;
+let counter = 50;
 const names = ["Pierre", "Leopold", "Joshua", "Basile", "Tom", "Elie", "Eliott", "Ethan", "Nathan", "Noah", "Gabriel", "Louis", "Raphael", "Jules", "Adam", "Leo", "Arthur", "Hugo", "Maël", "Lucas", "Liam", "Léo", "Evan", "Gabin", "Sacha", "Aaron", "Mathis", "Théo", "Enzo", "Antoine", "Maxime", "Paul", "Alexandre", "Nolan", "Valentin", "Timéo", "Tom", "Matteo", "Maxence", "Baptiste", "Rayan", "Clément", "Yanis", "Marius", "Victor", "Elias", "Isaac", "Mathéo", "Augustin", "Eliot", "Léon", "Esteban", "Nino", "Léandre"]
 
-setInterval(() => {
-    counter++;
-    socket.emit("add-score", {username: names[counter % names.length], cps: (Math.random() * 20).toFixed(2)})
-}, 3000)
+ setInterval(() => {
+     counter++;
+     socket.emit("add-score", {username: names[counter % names.length], cps: (Math.random() * 20).toFixed(2)})
+ }, 1000)
+
 
 
 socket.on("scores", (scores) => {
