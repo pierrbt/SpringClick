21a9cf5754e7b861d44d5f871c82779b69e2fb68
pierrbt <pierre08bt@gmx.co.uk>
2024-03-24 23:00:02 +0100

    feat: nice leaderboard, fix: confettis

--- leaderboard/src/Leaderboard.tsx
+++ leaderboard/src/Leaderboard.tsx
@@ -13,7 +13,9 @@ function Leaderboard() {
 	const [connected, setConnected] = useState(false);
 	const [editMode, setEditMode] = useState(false);
 	const [confetti, setConfetti] = useState(false);
+  const [previousLeader, setPreviousLeader] = useState(null);
 	const socketRef = useRef(null as Socket | null);
+
 	useEffect(() => {
 		const socket = io("ws://localhost:3000");
 		socketRef.current = socket;
@@ -35,17 +37,18 @@ function Leaderboard() {
 
   useEffect(() => {
     socketRef.current!.on("scores", (score) => {
-      console.log(score[0], scores[0])
       if (
         scores.length === 0 ||
         (score.length > 0 && !isEqual(score[0], scores[0]))
       ) {
-        console.log("wafrere")
-        setConfetti(true);
+        if (previousLeader !== null && !isEqual(score[0], previousLeader)) {
+          setConfetti(true);
+        }
+        setPreviousLeader(score[0]);
       }
       setScores(score);
     });
-  }, [socketRef.current, scores]);
+  }, [socketRef.current, scores, previousLeader]);
 
 	const toggleEditMode = () => {
 		setEditMode(!editMode);
@@ -72,41 +75,43 @@ function Leaderboard() {
 					<div id="leaderboard">
 						<div className="ribbon" />
 						<table>
-							{scores.map((score: Score, index: number) => (
-								<tr key={score.id} onMouseUp={toggleEditMode}>
-									<td className="number">{index + 1}</td>
-									<td className="name">{score.username}</td>
-									<td className="points">{score.cps.toFixed(2)}</td>
-									{editMode && (
-										<td>
-											<button
-												type="button"
-												onMouseUp={() => {
-													socketRef.current?.emit("remove-score", score.id);
-												}}
-											>
-												<svg
-													role="img"
-													aria-label="Delete"
-													xmlns="http://www.w3.org/2000/svg"
-													height="24"
-													viewBox="0 -960 960 960"
-													width="24"
-												>
-													<path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-												</svg>
-											</button>
-										</td>
-									)}
-								</tr>
-							))}
-							{scores.length === 0 && (
-								<tr>
-									<td className="name">
-										Soyez le premier à tenter votre chance !
-									</td>
-								</tr>
-							)}
+              <tbody>
+                {scores.map((score: Score, index: number) => (
+                  <tr key={score.id} onMouseUp={toggleEditMode}>
+                    <td className="number">{index + 1}</td>
+                    <td className="name">{score.username}</td>
+                    <td className="points"><span style={{color: "grey"}}>CPS</span>&nbsp;&nbsp;{score.cps.toFixed(2)}</td>
+                    {editMode && (
+                      <td>
+                        <button
+                          type="button"
+                          onMouseUp={() => {
+                            socketRef.current?.emit("remove-score", score.id);
+                          }}
+                        >
+                          <svg
+                            role="img"
+                            aria-label="Delete"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 -960 960 960"
+                            width="24"
+                          >
+                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                          </svg>
+                        </button>
+                      </td>
+                    )}
+                  </tr>
+                ))}
+                {scores.length === 0 && (
+                  <tr>
+                    <td className="name">
+                      Soyez le premier à tenter votre chance !
+                    </td>
+                  </tr>
+                )}
+              </tbody>
 						</table>
 					</div>
 				) : (
